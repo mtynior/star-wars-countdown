811c7965b57e5c642b80576e1348d1a55fd1fedc
Michal Tynior <michal.git@tynior.com>
2019-03-30 17:42:35 +0100

    Code refactoring

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,7 +34,9 @@ export default {
       return this.$store.getters.getBySlug(slug);
     },
     visibleTimers() {
-      return this.$store.getters.TIMERS.filter(timer => timer.slug != this.currentTimer.slug);
+      return this.$store.getters.TIMERS.filter(
+        timer => timer.slug != this.currentTimer.slug
+      );
     }
   }
 };
