d42b9747df20513adb62c952669f928a6fce904a
Michal Tynior <michal.git@tynior.com>
2019-04-03 13:00:58 +0200

    Add basic styles to 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
-    <h1>404 - Not Found!</h1>
+    <div class="not-found">
+      <h1>404</h1>
+      <h3>This is not the page you are looking for.</h3>
+    </div>
+    <TileGrid :timers="visibleTimers" class="timersGrid" />
+    <Footer />
   </div>
 </template>
 
 <script>
+import TileGrid from "@/components/TileGrid.vue";
+import Footer from "@/components/Footer.vue";
+
 export default {
   name: "NotFound",
+  components: {
+    TileGrid,
+    Footer
+  },
   metaInfo: {
-    title: "Star Wars Countdown",
-    titleTemplate: null
+    title: "Page not found",
+  },
+  computed: {
+    visibleTimers() {
+      return this.$store.getters.TIMERS;
+    }
   }
 };
 </script>
