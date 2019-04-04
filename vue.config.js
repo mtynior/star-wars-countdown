f9e3801e0c87bd6061cba8fe32dd597c4a4ee58f
Michal Tynior <michal.git@tynior.com>
2019-04-04 10:08:57 +0200

    Experminent

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
       skipWaiting: true,
       runtimeCaching: [
         {
-          urlPattern: /.*\.(?:png|jpg|jpeg|gif|webp|ico|svg|eot|ttf|woff)$/,
+          urlPattern: /.*\.(?:svg|jpg|jpeg|gif|webp|ico|png|eot|ttf|woff)$/,
           handler: "cacheFirst",
           options: {
             cacheName: "swc-assets",
